30b990378e93e74a32e359d06f385368f3292c7d
Luis Henrique Ferreira <luis.ferreira@safeweb.com.br>
2019-10-03 11:10:40 -0300

    Commit Back-End certo

---
+++ API/Controllers/AutenticarController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MDL;
+using CNG;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutenticarController : ControllerBase
+    {
+        [HttpGet]
+        [Route("dsa")]
+        public ActionResult<Usuario> Teste()
+        {
+
+            Usuario pessoa = new Usuario();
+            pessoa.NomeDoUsuario = "Luis Henrique";
+            return Ok(pessoa);
+        }
+
+
+        [HttpPost]
+        [Route("validaNomeUsuario")]
+        public ActionResult<string> ValidaNomeUsuario([FromBody]Usuario Usuario)
+        {
+            Usuario = UsuarioNG.Instancia.ValidaUsuario(Usuario);
+            return Ok(Usuario.NomeDoUsuario);
+        }
+    }
+}
